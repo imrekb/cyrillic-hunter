4a95a4f76e93e0dd2c456220f586bc38f786eaa2
Oleg Ulyanov <imrekbg@gmail.com>
2018-01-28 11:38:56 +0300

    feat: runs with output option

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@
 
 const program = require('commander');
 const run = require('./run');
+const fs = require('fs');
 
 program
     .version(process.env.npm_package_version)
     .usage('[options] <workingDir>')
 
-    .option('-o, --output', 'Output list of files in file')
+    .option('-o, --output [output]', 'Output list of files in file')
 
-    .option('-i, --include [maskIncluding...]', 'Mask of files for search cyrillic')
+    .option('-i, --include [maskIncluding]', 'Mask of files for search cyrillic')
 
-    .option('-e, --exclude [maskExcluding...]', 
+    .option('-e, --exclude [maskExcluding]', 
         'Mask for excluding files on search cyrillic [maskExcluding]')
 
     .action((workingDir) => {
@@ -24,7 +25,10 @@ program
                 });
 
                 if (program.output) {
-                    
+                    fs.writeFileSync(program.output, files.join('\r\n'), {
+                        encoding: 'utf8',
+                        flag: 'w'
+                    });
                 }
             });
     });
